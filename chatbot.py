c5cc2bc03d1040f7ffe1c585c1415d0ec66b7bee
oyvindhaugen <oyvindet@gmail.com>
2022-04-20 09:41:24 +0200

    a

--- chatbot.py
+++ chatbot.py
@@ -12,6 +12,17 @@ window.resizable(width=FALSE, height=FALSE)
 icon = PhotoImage(file ="icon_bot.png")
 window.iconphoto(False, icon)
 
+#menu
+menu_main = Menu(window)
+#edit menu
+menu_edit = Menu(window)
+menu_file = Menu(window)
 
+menu_main.add_cascade(label="File", menu=menu_file)
+menu_main.add_cascade(label="edit", menu=menu_edit)
+menu_file.add_command(label="Exit", command=window.destroy)
+#menu_edit.add_command(label="Dark Mode", command=#add dark mode function)
+#menu_edit.add_command(label="Clear History", command=#add clear function)
+window.config(menu=menu_main)
 
 window.mainloop()
