b785f5ab3511e50955f8ab37237efb4323227594
oyvindhaugen <oyvindet@gmail.com>
2022-04-20 09:29:39 +0200

    a

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
+#These are the basic settings for my window
 window = Tk()
 window.geometry("500x600")
+#window size
 window.title("Chatbot")
+#title
+window.resizable(width=FALSE, height=FALSE)
 
-#hello
+#window icon
+icon = PhotoImage(file ="icon_bot.png")
+window.iconphoto(False, icon)
+
+
 
 window.mainloop()
